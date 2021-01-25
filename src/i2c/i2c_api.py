358af1fd8cb3b77984022a883c049964472fcb6b
Marius Neuhalfen <m.e.neuhalfen@t-online.de>
2021-01-25 12:42:43 +0100

    added error catching in lib

--- src/i2c/i2c_api.py
+++ src/i2c/i2c_api.py
@@ -16,8 +16,10 @@ def get_bits(number,*args):
     for pos in args:
         if (number & (1 << pos)) > 0:
             res.append((number & (1 << pos)) // (number & (1 << pos)))
-        else:
+        elif (number & (1 << pos)) == 0:
             res.append(0)
+        else:
+            raise ValueError('Somehow got negative bit value!')
     return res if len(res) > 1 else res[0]
 
 def set_bits(number,dct):
@@ -25,7 +27,7 @@ def set_bits(number,dct):
     method to return number with bits in binary representation changed
 
     :param number: number to set bits to
-    :param dct: bits as keys with desired states as values
+    :param dct: bits as keys with desired states (0 or 1) as values
     :type number: int
     :type dct: dict
     
@@ -37,9 +39,11 @@ def set_bits(number,dct):
             number = number + 2**pos
         elif not val and get_bits(number,pos) > 0:
             number = number - 2**pos
+        elif val < 0:
+            raise ValueError('Cannot set bits to negative value!')
     return number
 
-def byte_write(i2c,address,register,subject):
+def byte_write(i2c,address,register,subject,print_change=True):
     """
     method to write new byte to i2c register
     
@@ -66,8 +70,8 @@ def byte_write(i2c,address,register,subject):
     i2c.writeto(address,bytes([register]))
     i2c.readfrom_into(address,changed)
 
-    print(f'{hex(register)} prev: {initial[0]:08b}\n{hex(register)} now:  {changed[0]:08b}\n')
-    
+    if print_change:
+        print(f'byte_write() on register {hex(register)}\nreg. prev: {initial[0]:08b} reg. now: {changed[0]:08b}\n')
     return None
 
 def byte_read(i2c,address,*args,len_array=1):
@@ -93,7 +97,6 @@ def byte_read(i2c,address,*args,len_array=1):
 
         #state gets sliced, each register has its place
         i2c.readfrom_into(address,state,start=len_array*ix,end=(len_array*ix)+len_array)
-     
     return state
     
 def byte_change(i2c,address,register,value,*args):
@@ -118,20 +121,28 @@ def byte_change(i2c,address,register,value,*args):
     original_byte = byte_read(i2c,address,register,len_array=1)
     original = struct.unpack('>B',original_byte)[0]
     
+    if min(args) < 0:
+        print(f'value: {value}, args: {args}, register: {register}')
+        raise ValueError('Negative value not allowed!\nBits in a byte indexed 0-7 from right to left.')
+        
+    elif value > 0 and len(args) != value.bit_length():
+        print(f'value: {value}, args: {args}, register: {register}')
+        raise ValueError('Please provide all / only those bits that will change with given value!\ne.g. value 5 in bit position 3 (00101000) requires bits 3,4,5 - indexed 0-7 from right to left.')
+        
+    for bit in args:
+        if bit != min(args) and bit - 1 not in args:
+            print(f'value: {value}, args: {args}, register: {register}')
+            raise ValueError('Please provide only the changing bits in consecutive order.\nUndefined behaviour will occur otherwise!')
+      
+    #shifting value into place
     base_byte = value << min(args)
     if value > 0:
         dct = {}
         for bit in args:
-            dct[bit] = get_bits(base_byte,bit)
+            dct[bit] = get_bits(base_byte,bit) #getting changed bits with given value
     else:
-        dct = {bit:0 for bit in args}
+        dct = {bit:0 for bit in args} #if value is 0 multiple bits may be set to 0
        
     new_byte = set_bits(original,dct)
-    
-    for bit in args:
-        if get_bits(new_byte,bit) != get_bits(base_byte,bit):
-            print(base_byte,new_byte,value,args)
-            raise ValueError('conversion did not work')
     byte_write(i2c,address,register,new_byte)
-    
     return None
