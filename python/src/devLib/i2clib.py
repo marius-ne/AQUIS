5d801ea2c31a20d17246318e441f03e0e2e8fe2a
Marius <m.e.neuhalfen@t-online.de>
2021-07-30 11:09:58 +0200

    last touches to LTC2934

--- python/src/devLib/i2clib.py
+++ python/src/devLib/i2clib.py
@@ -1,4 +1,4 @@
-def byteWrite(i2c,address: int,register: int,subject: int,printRes: bool=True):
+def byteWrite(i2c,address: int,register: int,subject: int,nReg: int=1,printRes: bool=True):
     """
     Method to write new byte to I2C register.
     
@@ -7,20 +7,26 @@ def byteWrite(i2c,address: int,register: int,subject: int,printRes: bool=True):
     i2c : i2c object from busio.I2C
     address : WHO_AM_I address of slave
     register : address of register to be written to
-    subject : number representation (endian-ness depends on slave) of new byte to be written
+    subject : number representation (endian-ness depends on slave) of new byte to be written.
+    nReg : determines length of bytes written to register, defaults to 1. 
+            If > 1 it must be checked that the device allows consecutive writing.
+    printRes: whether to print the result of the write or not (default is True)
     """
     # reading initial register state
-    initial = bytearray(1)
+    initial = bytearray(nReg)
     i2c.writeto(address,bytes([register]))
-    i2c.readfrom_into(address,initial)
+    for i in range(nReg):  
+        #state gets sliced, each register has its place
+        i2c.readfrom_into(address,initial,start=nReg*i,end=(nReg*i)+nReg)
      
-    
     i2c.writeto(address,bytes([register,subject]))
 
     # checking new state
-    changed = bytearray(1)
+    changed = bytearray(nReg)
     i2c.writeto(address,bytes([register]))
-    i2c.readfrom_into(address,changed)
+    for i in range(nReg):  
+        #state gets sliced, each register has its place
+        i2c.readfrom_into(address,changed,start=nReg*i,end=(nReg*i)+nReg)
 
     if printRes:
         print(f'byteWrite() on register {hex(register)}\nreg. prev: {initial[0]:08b} reg. now: {changed[0]:08b}\n')
@@ -34,8 +40,8 @@ def byteRead(i2c,address: int,register: int,nReg: int=1) -> bytearray:
     i2c : i2c object from busio.I2C
     address : WHO_AM_I address of slave
     register : register to be read
-    nReg : keyword argument to determine length of reading from each register, defaults to 1. 
-                If > 1 it must be checked that the device allows consecutive reading.
+    nReg : determines length of bytes read from each register, defaults to 1. 
+            If > 1 it must be checked that the device allows consecutive reading.
     """
     # nReg for each register if multiple registers
     state = bytearray(nReg)
--- python/src/devLib/ltc2943.py
+++ python/src/devLib/ltc2943.py
@@ -1,3 +1,4 @@
+from python.src.devLib.i2clib import byteChange
 import i2clib
 import struct
 import os
@@ -10,6 +11,10 @@ ADDR = 0x64
 CTRL = 0x01
 VOLT_H = 0x08
 TEMP_H = 0x14
+CHARGE_H = 0x02
+
+# current sensing resistor value in Ohm
+R_SENSE = 0.0011
 
 # datasheet: https://www.analog.com/media/en/technical-documentation/data-sheets/2943fa.pdf
 class LTC2943():
@@ -17,8 +22,12 @@ class LTC2943():
     def __init__(self,i2c):
         self.i2c = i2c # i2c object from busio.i2c
 
+        self.setPrescaler(2) # set prescaler to 16 (qLsb = 0.604)
+        # if Qbat <= 1978mAh (qLsb * 65535/2) then the charge register may start at 0x7FFF
+        self.setCharge(0x7FFF)
+
     # page 10
-    def setSleep(self,value: int):
+    def setADC(self,value: int):
         """
         Sets the operating mode of the ADC.
 
@@ -50,7 +59,17 @@ class LTC2943():
         -----
         value : 2 alert mode, 1 charge complete mode, 0 off"""
                                             # way to get bits from decimal
-        i2clib.byteChange(self.i2c,ADDR,CTRL,{2:value//2,1:value%2})
+        i2clib.byteChange(self.i2c,ADDR,CTRL,{2:value>>1,1:value%2})
+
+    # page 10
+    def setPrescaler(self, value: int):
+        """
+        Sets the value of the charge prescaler.
+        Depends upon capacity of battery and R_SENSE.
+
+        Provide decimal value of bits to be written.
+        """                                # getting single bits
+        i2clib.byteChange(self.i2c,ADDR,CTRL,{5:value>>2,4:(value>>1)%2,3:value%2})
 
     # page 13
     def voltage(self) -> int:
@@ -71,6 +90,29 @@ class LTC2943():
         raw = struct.unpack(">H",bytes)[0] # 2 bytes, big-endian, unsigned
 
         return 510 * (raw / 65535) # conversion according to datasheet
+
+    # page 13
+    def setCharge(self, value: int):
+        """
+        Sets the charge that the charge register will be counting from.
+        
+        Provide the register value in Hex.
+        """
+        # setting the two charge registers
+        i2clib.byteWrite(self.i2c,ADDR,CHARGE_H,value,2)
+
+    # page 13
+    def charge(self, qLsb: int) -> int:
+        """
+        Gets the charge from integrating current through
+        the sense resistor.
+
+        Needs the qLsb as determined by R_SENSE, Prescaler and Qbat.
+        """
+        bytes = i2clib.byteRead(self.i2c,ADDR,CHARGE_H,len_array=2)
+        raw = struct.unpack(">H",bytes)[0] # 2 bytes, big-endian, unsigned
+
+        return (raw / qLsb) # conversion according to datasheet
 
     
 
