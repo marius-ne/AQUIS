e2aa17a8088fae5493bc9ecea8dba0625d1a0099
Marius <m.e.neuhalfen@t-online.de>
2021-07-30 15:49:26 +0200

    added charge function to LTC2934

--- python/src/devLib/ltc2943.py
+++ python/src/devLib/ltc2943.py
@@ -13,8 +13,14 @@ VOLT_H = 0x08
 TEMP_H = 0x14
 CHARGE_H = 0x02
 
-# current sensing resistor value in Ohm
+# values to calculate charge
+REF_CHARGE = 0x7FFF # value of charge register at startup
 R_SENSE = 0.0011
+Q_START = 2000
+PRESC_VAL = 16
+PRESC_REG = 2 # register value to achieve PRESC_VAL
+# formula from page 11
+Q_LSB = 0.34 * ((50 / (R_SENSE*1000)) * (PRESC_VAL / 4096))
 
 # datasheet: https://www.analog.com/media/en/technical-documentation/data-sheets/2943fa.pdf
 class LTC2943():
@@ -22,9 +28,11 @@ class LTC2943():
     def __init__(self,i2c):
         self.i2c = i2c # i2c object from busio.i2c
 
-        self.setPrescaler(2) # set prescaler to 16 (qLsb = 0.604)
+        self.setADC(3)
+        self.setAlarm(2)
+        self.setPrescaler(2) # set prescaler to 16 (qLsb ~= 0.604)
         # if Qbat <= 1978mAh (qLsb * 65535/2) then the charge register may start at 0x7FFF
-        self.setCharge(0x7FFF)
+        self.setCharge(REF_CHARGE)
 
     # page 10
     def setADC(self,value: int):
@@ -72,6 +80,19 @@ class LTC2943():
         i2clib.byteChange(self.i2c,ADDR,CTRL,{5:value>>2,4:(value>>1)%2,3:value%2})
 
     # page 13
+    def setCharge(self, value: int):
+        """
+        Sets the charge that the charge register will be counting from.
+        
+        Provide the register value in Hex.
+        """
+        # Analog circuitry should be momentarily turned off (page 13)
+        self.setShutdown(1)
+        # setting the two charge registers
+        i2clib.byteWrite(self.i2c,ADDR,CHARGE_H,value,nReg=2)
+        self.setShutdown(0)
+
+    # page 13
     def voltage(self) -> int:
         """
         Gets the voltage (Volts) from the voltage registers.
@@ -92,27 +113,19 @@ class LTC2943():
         return 510 * (raw / 65535) # conversion according to datasheet
 
     # page 13
-    def setCharge(self, value: int):
-        """
-        Sets the charge that the charge register will be counting from.
-        
-        Provide the register value in Hex.
-        """
-        # setting the two charge registers
-        i2clib.byteWrite(self.i2c,ADDR,CHARGE_H,value,2)
-
-    # page 13
-    def charge(self, qLsb: int) -> int:
+    def charge(self) -> int:
         """
         Gets the charge from integrating current through
         the sense resistor.
 
-        Needs the qLsb as determined by R_SENSE, Prescaler and Qbat.
+        Needs the Charge that the module is referencing from. This means
+        the start value of the register interpreted as Charge.
         """
         bytes = i2clib.byteRead(self.i2c,ADDR,CHARGE_H,len_array=2)
         raw = struct.unpack(">H",bytes)[0] # 2 bytes, big-endian, unsigned
 
-        return (raw / qLsb) # conversion according to datasheet
+        # delta to refCharge, then scaling with lsb, then subtracting from starting voltage
+        return (Q_START - ((REF_CHARGE - raw) * Q_LSB))
 
     
 
